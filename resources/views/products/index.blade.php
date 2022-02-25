59ada82c0c931c13276170b549bc7edaeba61183
Kyuuto10 <ahamd.pikachu@gmail.com>
2022-02-25 09:18:39 +0700

    modified title

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -29,7 +29,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 8 CRUD with Image Upload Example from scratch - ItSolutionStuff.com</h2>
+                <h2>Laravel 8 CRUD with Image</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
